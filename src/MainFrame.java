1c4835eef72c89bebe6b58359c83b2a70c683373
Artyom <fanny.koob@gmail.com>
2020-12-10 17:36:12 +0300

    mainFrame1.1

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -29,5 +29,76 @@ public class MainFrame extends JFrame {
     private JMenuItem saveToTextMenuItem;
     private JFileChooser fileChooser = null;
 
+    public MainFrame() {
+        super("Вывод графика функции");
+        this.setSize(600, 600);
+        Toolkit kit = Toolkit.getDefaultToolkit();
+        this.setLocation((kit.getScreenSize().width - 600) / 2, (kit.getScreenSize().height - 600) / 2);
+        JMenuBar menuBar = new JMenuBar();
+        this.setJMenuBar(menuBar);
+        JMenu fileMenu = new JMenu("Файл");
+        menuBar.add(fileMenu);
+
+        Action openGraphicsAction = new AbstractAction("Открыть файл") {
+            public void actionPerformed(ActionEvent arg0) {
+                if (MainFrame.this.fileChooser == null) {
+                    MainFrame.this.fileChooser = new JFileChooser();
+                    MainFrame.this.fileChooser.setCurrentDirectory(new File("."));
+                }
+
+                if (MainFrame.this.fileChooser.showOpenDialog(MainFrame.this) == 0) {
+                }
+
+                MainFrame.this.openGraphics(MainFrame.this.fileChooser.getSelectedFile());
+            }
+        };
+        fileMenu.add(openGraphicsAction);
+
+        Action saveToTextAction = new AbstractAction("Сохранить в .txt") {
+            public void actionPerformed(ActionEvent arg0) {
+                if (MainFrame.this.fileChooser == null) {
+                    MainFrame.this.fileChooser = new JFileChooser();
+                    MainFrame.this.fileChooser.setCurrentDirectory(new File("."));
+                }
+
+                if (MainFrame.this.fileChooser.showSaveDialog(MainFrame.this) == 0) {
+                    MainFrame.this.display.saveToTextFile(MainFrame.this.fileChooser.getSelectedFile());
+                }
+
+            }
+        };
+        this.saveToTextMenuItem = fileMenu.add(saveToTextAction);
+        JMenu graphicsMenu = new JMenu("График");
+        menuBar.add(graphicsMenu);
+
+        Action showAxisAction = new AbstractAction("Показать оси координат") {
+            public void actionPerformed(ActionEvent e) {
+                MainFrame.this.display.setShowAxis(MainFrame.this.showAxisMenuItem.isSelected());
+            }
+        };
+        this.showAxisMenuItem = new JCheckBoxMenuItem(showAxisAction);
+        graphicsMenu.add(this.showAxisMenuItem);
+        this.showAxisMenuItem.setSelected(true);
+        Action showMarkersAction = new AbstractAction("Показать маркеры точек") {
+            public void actionPerformed(ActionEvent e) {
+                MainFrame.this.display.setShowMarkers(MainFrame.this.showMarkersMenuItem.isSelected());
+            }
+        };
+        this.showMarkersMenuItem = new JCheckBoxMenuItem(showMarkersAction);
+        graphicsMenu.add(this.showMarkersMenuItem);
+        this.showMarkersMenuItem.setSelected(true);
+        graphicsMenu.addSeparator();
+        Action resetGraphicsAction = new AbstractAction("Отменить все изменения") {
+            public void actionPerformed(ActionEvent event) {
+                MainFrame.this.display.reset();
+            }
+        };
+        this.resetGraphicsMenuItem = new JMenuItem(resetGraphicsAction);
+        graphicsMenu.add(this.resetGraphicsMenuItem);
+        this.resetGraphicsMenuItem.setEnabled(false);
+        graphicsMenu.addMenuListener(new MainFrame.GraphicsMenuListener());
+        this.getContentPane().add(this.display, "Center");
+    }
+
    
 }
